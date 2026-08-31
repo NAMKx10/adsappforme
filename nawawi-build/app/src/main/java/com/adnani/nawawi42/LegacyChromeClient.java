169973931ad5814522ef23c331d7bb784fd825a9
NAMKx10 <najix10@gmail.com>
2026-08-31 17:25:06 +0300

    Nawawi Test 001: Android 4.4 file chooser

---
+++ nawawi-build/app/src/main/java/com/adnani/nawawi42/LegacyChromeClient.java
@@ -0,0 +1,13 @@
+package com.adnani.nawawi42;
+
+import android.net.Uri;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+
+public class LegacyChromeClient extends WebChromeClient {
+    private final MainActivity activity;
+    public LegacyChromeClient(MainActivity activity) { this.activity = activity; }
+    public void openFileChooser(ValueCallback<Uri> uploadMsg) { activity.launchLegacyFileChooser(uploadMsg); }
+    public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) { activity.launchLegacyFileChooser(uploadMsg); }
+    public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) { activity.launchLegacyFileChooser(uploadMsg); }
+}
